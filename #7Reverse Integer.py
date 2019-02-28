bd0cded5269b1ba199cee32a27f5db06f7778ab3
CLAY2333 <CLAY2333@163.com>
2019-02-28 13:15:29 +0800

    happy

---
+++ #7Reverse Integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        if(x>0):
+            x=str(x)
+            y = list(x)
+            for i in range(len(x)):
+                y[i]=x[len(x)-i-1]
+            s=''.join(y)
+            s=int(s)
+            if(s>2147483648):
+                return 0
+            return s
+        else:
+            x=x*(-1)
+            x = str(x)
+            y = list(x)
+            for i in range(len(x)):
+                y[i]=x[len(x)-i-1]
+            s = ''.join(y)
+            s = int(s)
+            s=s*(-1)
+            if(s<-2147483648):
+                return 0
+            return s
---
+++ #9Palindrome Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x>=0):
+            if(x==0):
+                 return True
+            x=str(x)
+            y = list(x)
+            for i in range(len(x)):
+                y[i]=x[len(x)-i-1]
+            s=''.join(y)
+            s=int(s)
+            if(s==int(x)):
+                return True
+            else:
+                return False
+        else:
+           return False
