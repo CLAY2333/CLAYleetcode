6184f5f34ebdc4cab249eb3b26534717d86c423c
CLAY2333 <CLAY2333@163.com>
2019-02-28 13:05:59 +0800

    Create Two Sum#1.py

---
+++ Two Sum#1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, num, target):
+        dict = {}
+        for i in xrange(len(num)):
+            x = num[i]
+            if target-x in dict:
+                return (dict[target-x], i)
+            dict[x] = i
