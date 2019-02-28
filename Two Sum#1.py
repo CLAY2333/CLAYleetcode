520a3a28188f8710597df5beecbc6f323378ad6c
CLAY2333 <CLAY2333@163.com>
2019-02-28 13:15:39 +0800

    Delete Two Sum#1.py

--- Two Sum#1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, num, target):
-        dict = {}
-        for i in xrange(len(num)):
-            x = num[i]
-            if target-x in dict:
-                return (dict[target-x], i)
-            dict[x] = i
