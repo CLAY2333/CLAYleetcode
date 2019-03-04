0ac0f9bcf2a1ac50a4355f44c3005a1f7d0c4479
CLAY2333 <CLAY2333@163.com>
2019-03-04 22:12:04 +0800

    Create #21Merge Two Sorted Lists.py

---
+++ #21Merge Two Sorted Lists.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+ class ListNode(object):
+     def __init__(self, x, p):
+         self.val = x
+         self.next =None
+     def nextNode(self,p):
+         s=ListNode()
+
+
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+if __name__=='__main__':
+    s=Solution()
