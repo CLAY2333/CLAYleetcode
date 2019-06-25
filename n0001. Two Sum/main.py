64e076456b613dacdc4a56824db7134ab747690a
CLAY2333 <CLAY2333@163.com>
2019-06-25 16:05:33 +0800

    commit by crawler.py @CLAY at 2019-06-25 16:05

--- n0001. Two Sum/main.py
+++
@@ -1,9 +0,0 @@
-// Author: CLAY2333 @ https://github.com/CLAY2333/CLAYleetcode
-class Solution:
-    def twoSum(self, num, target):
-        dict = {}
-        for i in xrange(len(num)):
-            x = num[i]
-            if target-x in dict:
-                return (dict[target-x], i)
-            dict[x] = i
